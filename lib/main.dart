dd90efe9b0719e417493878db5ff56f91203f229
Nurdiana <84701259+Nurdiananur@users.noreply.github.com>
2022-03-20 11:03:49 +0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,83 @@
 
-import 'package:doc_express/screens/splash_screen.dart';
+import 'dart:async';
+import 'package:doc_express/login.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-
-  runApp(MyApp());
+void main() {
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: SplashScreen(),
-      debugShowCheckedModeBanner: false,
-      color: Colors.indigo[900],
+    return ScreenUtilInit(
+      designSize: const Size(360, 690),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: () => MaterialApp(
+        //... other code
+        builder: (context, widget) {
+          //add this line
+          ScreenUtil.setContext(context);
+          return MediaQuery(
+            //Setting font does not change with system font size
+            data: MediaQuery.of(context).copyWith(textScaleFactor: 1.0),
+            child: widget!,
+          );
+        },
+        theme: ThemeData(
+          textTheme: TextTheme(
+            //To support the following, you need to use the first initialization method
+              button: TextStyle(fontSize: 45.sp)),
+        ),
+        home: SplashScreen(),
+      ),
+    );
+  }
+}
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(
+        Duration(seconds: 5),
+            () => Navigator.pushReplacement(
+            context, MaterialPageRoute(builder: (context) => LoginScreen())));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body:
+      Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+
+            SizedBox(),
+            Image.asset(
+                'assets/doc.jpg',
+                height: 171.0,
+                width: 72.0
+            ),
+
+            Text('Добро пожаловать в Doce ',style: TextStyle(fontSize: 16),),
+          ],
+        ),
+      ),
     );
   }
 }
